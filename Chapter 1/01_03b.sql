4437357d609281aea961e8b72525a6390ebd6b50
Anuj Kumar Gautam <61941950+theanujgautam@users.noreply.github.com>
2024-03-29 09:21:09 +0000

    Add New Product

--- Chapter 1/01_03b.sql
+++ Chapter 1/01_03b.sql
@@ -1,1 +1,20 @@
+-- Add new product to the product table
 -- Insert new grape flavor product into table
+INSERT INTO Product(
+    ProductID,
+    productCode,
+    ProductName,
+    Size,
+    Variety,
+    Price,
+    Status
+  )
+VALUES(
+    17,
+    'MWPRA20',
+    'Mineral Water',
+    20,
+    'Grape',
+    '1.79',
+    'ACTIVE'
+  );
