f230b80274d03286785a171fa3914ec4dd752a02
Anuj Kumar Gautam <61941950+theanujgautam@users.noreply.github.com>
2024-04-01 12:17:29 +0000

    Remove missing values

--- Chapter 1/01_06b.sql
+++ Chapter 1/01_06b.sql
@@ -1,1 +1,10 @@
 -- Remove null values from customer table
+-- Making sure no data send with missing contact detail for marketing campaing
+
+SELECT
+FirstName,
+LastName,
+Email,
+Phone
+FROM Customer
+WHERE Email IS NOT NULL AND PHONE IS NOT NULL;
