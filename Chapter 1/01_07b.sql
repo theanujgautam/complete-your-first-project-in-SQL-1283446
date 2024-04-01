209ec06ac7b6c9f21d0ca68b4d1e599cfe61935f
Anuj Kumar Gautam <61941950+theanujgautam@users.noreply.github.com>
2024-04-01 12:30:26 +0000

    Create new months columns

--- Chapter 1/01_07b.sql
+++ Chapter 1/01_07b.sql
@@ -1,1 +1,7 @@
 -- Create new month columns
+-- retrieve their order data along with the months when orders were made
+
+SELECT * ,
+MONTH(CreationDate) AS MonthNumber,
+MONTHNAME(CreationDate) AS MonthName
+FROM Orders;
